d67e0334c52cea396560e7abd3f7bfe1bac7de66
Jang <laganka@naver.com>
2015-08-26 15:47:30 +0900

    invent 게시판 추가 코딩 완료 추후 3d 나 sketch article 코딩완료시 연결 만 하면됨 연결 고리는 javascript 에 따로 적어놨음...

---
+++ src/main/java/com/sajo/dto/InventDTO.java
@@ -0,0 +1,31 @@
+package com.sajo.dto;
+
+public class InventDTO {
+	
+	private String imgnum,gno,gnum;
+
+	public String getImgnum() {
+		return imgnum;
+	}
+
+	public void setImgnum(String imgnum) {
+		this.imgnum = imgnum;
+	}
+
+	public String getGnum() {
+		return gnum;
+	}
+
+	public void setGnum(String gnum) {
+		this.gnum = gnum;
+	}
+
+	public String getGno() {
+		return gno;
+	}
+
+	public void setGno(String gno) {
+		this.gno = gno;
+	}
+	
+}
